4b0797c12c95829b586903bd34f38535eb09630f
kuanyi0226 <kuanyi0226@gmail.com>
2022-10-05 09:49:00 +0800

    print add content

--- lesson1_print.dart
+++ lesson1_print.dart
@@ -8,4 +8,8 @@ void main() {
   //print換行
 
   //利用 $ 來print，若很複雜，可以在$後面加{}把要印的變數包起來
+  var myMoney = 0;
+  print("Total money in my wallet is $myMoney");
+  var debt = 10000;
+  print('I\'ve got a loan from bank, now my total money is ${myMoney - debt}');
 }
